d9b234436299ecb5e9b797013475306fc4c26593
Cody Tope <codytope@gmail.com>
2023-01-15 09:08:17 -0500

    Initial Commit 1.7

--- part1/1.6-1.11/src/App.js
+++ part1/1.6-1.11/src/App.js
@@ -23,13 +23,18 @@ const Feedback = ({incGood, incNeutral, incBad}) => {
   )
 }
 
-const Statistics = ({good, neutral, bad}) =>{
+const Statistics = ({good, neutral, bad, all, average, positive}) =>{
   return(
     <>
       <h1>statistics</h1>
-      <p>good {good} <br />
-      neutral {neutral} <br />
-      bad {bad}</p>
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br />
+        all {all} <br />
+        average {average} <br />
+        positive {positive}
+      </p>
     </>
   )
 }
@@ -38,23 +43,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
   
   const incGood = () => {
     setGood(good+1);
+    setAll(all+1);
+    setAverage((good-bad+1)/(all+1));
+    setPositive((good+1)/(all+1));
   }
 
   const incNeutral = () => {
     setNeutral(neutral+1);
+    setAll(all+1);
+    setAverage((good-bad)/(all+1));
+    setPositive((good)/(all+1));
   }
 
   const incBad = () => {
     setBad(bad+1);
+    setAll(all+1);
+    setAverage((good-bad-1)/(all+1));
+    setPositive((good)/(all+1));
   }
 
   return (
     <div>
       <Feedback incGood={incGood} incNeutral={incNeutral} incBad={incBad}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/> 
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}/> 
     </div>
   );
 }
