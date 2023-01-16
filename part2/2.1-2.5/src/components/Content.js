4c27177d5724743469ea262f2293f6e2368878ba
Cody Tope <codytope@gmail.com>
2023-01-16 16:30:55 -0500

    Initial Commit 2.1

---
+++ part2/2.1-2.5/src/components/Content.js
@@ -0,0 +1,13 @@
+import Part from './Part';
+
+const Content = ({parts}) => {
+    return(
+      <>
+        {parts.map(part => 
+            <Part key={part.id} name={part.name} exercises={part.exercises} />
+        )}
+      </>
+    )
+  }
+
+  export default Content;
