a7f1e6828f6b7d3a063395d91ea13195cdc35078
Cody Tope <codytope@gmail.com>
2023-01-17 05:25:45 -0500

    Initial Commit 2.4 & 2.5

--- part2/2.1-2.5/src/App.js
+++ part2/2.1-2.5/src/App.js
@@ -1,9 +1,9 @@
 import Course from './components/Course';
 
 const App = () => {
-  const course = {
-    id: 1,
+  const courses =  [{
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
@@ -19,12 +19,36 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
-  }
+  }, 
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2
+      }
+    ]
+  }]
 
   return (
-  <Course course={course} />
+    <>
+      <h1>Web development cirriculum</h1>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </>
   )
 }
 
