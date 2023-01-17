37d32559edf456eae854bdd0a3ef93c30bd463b6
Cody Tope <codytope@gmail.com>
2023-01-17 05:15:20 -0500

    Initial Commit 2.2 & 2.3

--- part2/2.1-2.5/src/components/Content.js
+++ part2/2.1-2.5/src/components/Content.js
@@ -1,11 +1,16 @@
 import Part from './Part';
 
 const Content = ({parts}) => {
+    const totalExercises = parts.reduce((sum, part) => {
+      return sum + part.exercises;
+    }, 0)
+    
     return(
       <>
         {parts.map(part => 
             <Part key={part.id} name={part.name} exercises={part.exercises} />
         )}
+        <b>Total of {totalExercises} exercises</b>
       </>
     )
   }
